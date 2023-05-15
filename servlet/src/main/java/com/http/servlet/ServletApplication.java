66889d9e20b0ff0bde7a1b5bc3f94f039a54ddab
TayLee <thlee991@gmail.com>
2023-05-15 15:45:21 +0900

    commit) servletStudy

---
+++ servlet/src/main/java/com/http/servlet/ServletApplication.java
@@ -0,0 +1,13 @@
+package com.http.servlet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServletApplication.class, args);
+	}
+
+}
