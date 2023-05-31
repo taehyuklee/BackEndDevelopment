543c926ced94e8defa3a559b5a04bcb5b31bd70a
TAYLEE <89365465+TAEHYUKLEE@users.noreply.github.com>
2023-05-31 23:45:50 +0900

    commit) decoratorPattern chain

--- SpringFramework/SpringAdvanced/proxypattern/src/test/java/design/pattern/proxy/pureproxy/decorator/DecoratorPatternTest.java
+++ SpringFramework/SpringAdvanced/proxypattern/src/test/java/design/pattern/proxy/pureproxy/decorator/DecoratorPatternTest.java
@@ -6,6 +6,7 @@ import design.pattern.proxy.pureproxy.decorator.code.Component;
 import design.pattern.proxy.pureproxy.decorator.code.DecoratorPatternClient;
 import design.pattern.proxy.pureproxy.decorator.code.MessageDecorator;
 import design.pattern.proxy.pureproxy.decorator.code.RealComponent;
+import design.pattern.proxy.pureproxy.decorator.code.TimeDecorator;
 
 public class DecoratorPatternTest {
     
@@ -25,4 +26,15 @@ public class DecoratorPatternTest {
         client.execute();
     }
     //코드를 수정하지 않고 로그를 남기는거 -> Client와 Target객체는 아무 변화가 없다. 하지만 프록시 패턴을 사용해서 중간에 로그를 남기거나 꾸미는게 가능해졌다.
+
+    //여러개의 Decorator proxy객체를 이용해서 꾸며볼 것이다.
+    @Test
+    void decorator2(){
+        Component realComponent = new RealComponent();
+        Component messageDecorator = new MessageDecorator(realComponent);
+        Component timeDecorator = new TimeDecorator(messageDecorator); //timeDecorator가 messageDecorator를 부르고 messageDecorator가 Target을 부른다. 즉 두 번 꾸미게 됨.
+        DecoratorPatternClient client = new DecoratorPatternClient(timeDecorator); 
+        client.execute();
+    }
+
 }
---
+++ SpringFramework/SpringAdvanced/proxypattern/src/test/java/design/pattern/proxy/pureproxy/decorator/code/TimeDecorator.java
@@ -0,0 +1,29 @@
+package design.pattern.proxy.pureproxy.decorator.code;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class TimeDecorator implements Component{
+
+    private Component component;
+
+    public TimeDecorator(Component component){
+        this.component = component;
+    }
+
+
+    @Override
+    public String operation() {
+        log.info("TimeDecorator 실행");
+        long startTime = System.currentTimeMillis();
+
+        String realData = component.operation();
+
+        long endTime = System.currentTimeMillis();
+        long resultTime = endTime - startTime;
+        log.info("TimeDecorator 종료 resultTime={}ms", resultTime);
+
+        return realData;
+    }
+    
+}
