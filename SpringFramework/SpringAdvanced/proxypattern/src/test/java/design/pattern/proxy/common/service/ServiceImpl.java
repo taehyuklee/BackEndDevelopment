f9666e396f2664d743a66a185ed19324174824f4
TAYLEE <89365465+TAEHYUKLEE@users.noreply.github.com>
2023-06-04 14:02:58 +0900

    commit) again

---
+++ SpringFramework/SpringAdvanced/proxypattern/src/test/java/design/pattern/proxy/common/service/ServiceImpl.java
@@ -0,0 +1,18 @@
+package design.pattern.proxy.common.service;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class ServiceImpl implements ServiceInterface{
+
+    @Override
+    public void save() {
+        log.info("save 호출");
+    }
+
+    @Override
+    public void find() {
+        log.info("find 호출");
+    }
+    
+}
---
+++ SpringFramework/SpringAdvanced/proxypattern/src/test/java/design/pattern/proxy/common/service/ServiceInterface.java
@@ -0,0 +1,8 @@
+package design.pattern.proxy.common.service;
+
+public interface ServiceInterface {
+    
+    void save();
+
+    void find();
+}
