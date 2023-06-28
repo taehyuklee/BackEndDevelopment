319c04f8e2ba691be1dea2db2101675f550004ca
TayLee <thlee991@gmail.com>
2023-06-29 01:01:49 +0900

    commit and push 올려다오

--- Partitioning/src/main/java/db/partition/partitioning/PartitionService.java
+++ Partitioning/src/main/java/db/partition/partitioning/PartitionService.java
@@ -3,8 +3,10 @@ package db.partition.partitioning;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Service;
 
 @Slf4j
+@Service
 @RequiredArgsConstructor
 public class PartitionService {
 
@@ -12,7 +14,7 @@ public class PartitionService {
 
     public void createTraffic(){
 
-        String createStatesment = "create table Traffic (id Long, startTime LocalDateTime, endTime LocalDateTime, contents varchar)";
+        String createStatesment = "create table Traffic (id Long, startTime TimeStamp endTime TimeStamp, contents varchar)";
 
         log.info("새로운 Partition Table 생성을 시작합니다.");
         template.execute(createStatesment);
--- Partitioning/src/main/java/db/partition/traffic/controller/TrafficController.java
+++ Partitioning/src/main/java/db/partition/traffic/controller/TrafficController.java
@@ -1,10 +1,21 @@
 package db.partition.traffic.controller;
 
+import db.partition.partitioning.PartitionService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
+@RequestMapping("/partition")
 public class TrafficController {
 
+    private final PartitionService partitionService;
 
+    @GetMapping("/traffic")
+    public void createTable(){
+        partitionService.createTraffic();
+    }
 
 }
