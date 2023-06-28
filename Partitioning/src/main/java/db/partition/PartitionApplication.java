ff918794c8f0fec4810de26a493c603d5789a27a
TayLee <thlee991@gmail.com>
2023-06-28 10:50:19 +0900

    commit and push 올려다오

---
+++ Partitioning/src/main/java/db/partition/PartitionApplication.java
@@ -0,0 +1,13 @@
+package db.partition;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PartitionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PartitionApplication.class, args);
+	}
+
+}
---
+++ Partitioning/src/test/java/db/partition/PartitionApplicationTests.java
@@ -0,0 +1,13 @@
+package db.partition;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PartitionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
