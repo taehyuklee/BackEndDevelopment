135854891b22cbe62571ac75689fcef698868914
TAYLEE <89365465+TAEHYUKLEE@users.noreply.github.com>
2023-06-30 01:52:48 +0900

    commit) partitionNaming

--- Partitioning/src/main/java/db/partition/partitioning/service/PartitionService.java
+++ Partitioning/src/main/java/db/partition/partitioning/service/PartitionService.java
@@ -18,19 +18,19 @@ public class PartitionService {
 
     public void createTraffic(){
 
-        String statesment = "create table public.\"TRAFFIC\" (id bigint, startTime timeStamp, endTime timeStamp, contents varchar(50)) partition by range (endtime)";
+        String statement = "create table public.\"TRAFFIC\" (id bigint, startTime timeStamp, endTime timeStamp, contents varchar(50)) partition by range (endtime)";
 
         log.info("새로운 Partition Table 생성을 시작합니다.");
-        template.execute(statesment);
+        template.execute(statement);
         log.info("Partition Table 생성이 종료되었습니다.");
     }
 
     public void deleteTraffic(){
 
-        String statesment = "drop table public.\"TRAFFIC\"";
+        String statement = "drop table public.\"TRAFFIC\"";
 
         log.info("Partition Table 삭제를 시작합니다.");
-        template.execute(statesment);
+        template.execute(statement);
         log.info("Partition Table 삭제했습니다.");
     }
 
@@ -39,21 +39,21 @@ public class PartitionService {
         LocalDate currentDate = LocalDate.now();
         String startDate = PartitionUtility.getStartOfMonth(currentDate);
         String endDate = PartitionUtility.getEndOfMonth(currentDate);
-        String partionName = "집에 가고싶다";
+        String partitionName = PartitionUtility.getPartitionName(currentDate);
 
-        String statesment = "create table public.\"TRAFFIC_ "+partionName+" \" partition of public.\"TRAFFIC\" for values from ('" + startDate + "') to ('"  +endDate + "')";
+        String statement = "create table public.\"TRAFFIC_"+partitionName+"\" partition of public.\"TRAFFIC\" for values from ('"+startDate+"') to ('"+endDate+"')";
 
         log.info("새로운 Partition Table 생성을 시작합니다.");
-        template.execute(statesment);
+        template.execute(statement);
         log.info("Partition Table 생성이 종료되었습니다.");
     }
 
     public void deletePartition(){
 
-        String statesment = "drop table if exists public.\"TRAFFIC_집에 가고싶다\"";
+        String statement = "drop table if exists public.\"TRAFFIC_집에 가고싶다\"";
 
         log.info("새로운 Partition Table 삭제를 시작합니다.");
-        template.execute(statesment);
+        template.execute(statement);
         log.info("Partition Table 삭제가 종료되었습니다.");
     }
 }
