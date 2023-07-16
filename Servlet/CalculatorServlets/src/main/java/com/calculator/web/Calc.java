dccb5ec930fceafd8b1cd1a8dda8bac3a12d60b6
TayLee <89365465+taehyuklee@users.noreply.github.com>
2023-07-17 00:36:27 +0900

    calculoatir

--- Servlet/CalculatorServlets/src/main/java/com/calculator/web/Calc.java
+++ Servlet/CalculatorServlets/src/main/java/com/calculator/web/Calc.java
@@ -7,14 +7,23 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet("/add")
-public class Add extends HttpServlet {
+@WebServlet("/calc")
+public class Calc extends HttpServlet {
 
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
 		//Encoding decoding과 형식을 모두 UTF-8로 통일한다.
 		response.setCharacterEncoding("UTF-8");
 		response.setContentType("text/html; charset=UTF-8");
+		
+		//input form에서 name="operator"의 value값을 보고 "덧셈", "뺄셈"을 구분할수 있다. 
+		String op = request.getParameter("operator");
+		
+		/*
+		 * <input type="submit" name="operator" value="덧셈" />
+		 * 여기서 value의 key를 operator로 넘겨주게 되는 형식이다 즉,input button의 정보를 넘겨주게 됨. \
+		 * 이게 없으면 x, y값만 넘어옴
+		 * */
 		
 		//들어오는 건 무조건 문자열로 받아온다고 함. 
 		String x_ = request.getParameter("x");
@@ -27,7 +36,12 @@ public class Add extends HttpServlet {
 		if(!x_.equals("")) x = Integer.parseInt(x_);
 		if(!y_.equals("")) y = Integer.parseInt(y_);
 		
-		int result = x+y;
+		int result = 0;
+		
+		if(op.equals("덧셈"))
+			result = x+y;
+		else
+			result = x-y;
 		
 		response.getWriter().printf("result is %d\n", result);
 
