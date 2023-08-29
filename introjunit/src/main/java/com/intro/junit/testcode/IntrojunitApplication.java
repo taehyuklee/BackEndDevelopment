c8b7f91e559b7252555f85a6ba4c0b41583a2a7e
TayLee <thlee991@gmail.com>
2023-08-29 11:19:10 +0900

    commit) Junit test code 시작

---
+++ introjunit/src/main/java/com/intro/junit/testcode/IntrojunitApplication.java
@@ -0,0 +1,13 @@
+package com.intro.junit.testcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntrojunitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntrojunitApplication.class, args);
+    }
+
+}
---
+++ introjunit/src/test/java/com/intro/junit/testcode/IntrojunitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.intro.junit.testcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntrojunitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
