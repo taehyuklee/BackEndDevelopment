dccb675a11c5041aa97b0376f256aa6f2debdb65
TayLee <thlee991@gmail.com>
2023-09-08 14:49:59 +0900

    commit) static method test

--- Introjunit/src/main/java/com/intro/junit/testcode/first_service/service/GoodService.java
+++ Introjunit/src/main/java/com/intro/junit/testcode/first_service/service/GoodService.java
@@ -1,5 +1,6 @@
 package com.intro.junit.testcode.first_service.service;
 
+import com.intro.junit.testcode.first_service.utility.GoodUtility;
 import lombok.NoArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -9,6 +10,19 @@ public class GoodService {
 
     public void create(){
 
+    }
+
+    public void read(){
+
+    }
+
+    public void useUtility(){
+        System.out.println("hi");
+        String[] parsed = GoodUtility.parsingMethod("a b c");
+        System.out.println("good");
+        for(int i=0; i<3; i++){
+            System.out.println(parsed[i]);
+        }
     }
 
 }
