ba37d90f7c47701cadd7d38da777e14801ea1f7e
TAYLEE <89365465+TAEHYUKLEE@users.noreply.github.com>
2024-03-18 00:03:13 +0900

    MARIA DB config

---
+++ maria/src/main/java/maria/practice/usersvc/domain/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package maria.practice.usersvc.domain.repository;
+
+import maria.practice.usersvc.domain.entity.UserInfo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.*;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserInfo, Long> {
+
+}
