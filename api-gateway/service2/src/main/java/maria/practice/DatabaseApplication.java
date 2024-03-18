037cc5d9cf719670ffc2f7787209e771d7112018
taylee <thlee991@gmail.com>
2024-03-18 17:26:56 +0900

    up gateway practice

--- api-gateway/service2/src/main/java/maria/practice/DatabaseApplication.java
+++ api-gateway/service2/src/main/java/maria/practice/DatabaseApplication.java
@@ -2,8 +2,10 @@ package maria.practice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient //Eureka Discovery Client로서 등록
 public class DatabaseApplication {
 
 	public static void main(String[] args) {
