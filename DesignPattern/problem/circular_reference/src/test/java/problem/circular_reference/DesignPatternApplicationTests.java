b4850260d865162e481d2d2df6cbff5857e6e671
TayLee <thlee991@gmail.com>
2024-05-05 11:52:33 +0900

    commit) 순환참조 풀이

---
+++ DesignPattern/problem/circular_reference/src/test/java/problem/circular_reference/DesignPatternApplicationTests.java
@@ -0,0 +1,13 @@
+package problem.circular_reference;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignPatternApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
