bffd4a12abdc76f6063d6ce91166ed538f7e0e9e
TayLee <thlee991@gmail.com>
2024-05-05 12:54:52 +0900

    commit

--- DesignPattern/problem/circular_reference/src/main/java/problem/circular_reference/user_mngt/service/impl/UserServiceImpl.java
+++ DesignPattern/problem/circular_reference/src/main/java/problem/circular_reference/user_mngt/service/impl/UserServiceImpl.java
@@ -1,10 +1,15 @@
 package problem.circular_reference.user_mngt.service.impl;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import problem.circular_reference.user_mngt.service.UserService;
+import problem.circular_reference.usergrp_mngt.service.UserGrpService;
 
 @Service
+@RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
+
+    private final UserGrpService userGrpService;
 
     @Override
     public void createUser(){
