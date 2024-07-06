01c8823c01d631a7f90565cea34efc203170dddb
TayLee <89365465+taehyuklee@users.noreply.github.com>
2024-07-06 12:47:07 +0900

    Add files via upload

---
+++ ApiFunctions/ConvertOfPowerPoint/ppt/src/test/java/com/converter/ppt/PptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.converter.ppt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
