7a8e026aea7bb40b3c1b55e75f772d2377d5d8ac
TAYLEE <89365465+TAEHYUKLEE@users.noreply.github.com>
2025-05-18 23:54:43 +0900

    commit) auth-core

---
+++ auth-core/src/main/java/com/taehyuk/_auth/auth_core/AuthCoreApplication.java
@@ -0,0 +1,13 @@
+package com.taehyuk._auth.auth_core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthCoreApplication.class, args);
+	}
+
+}
