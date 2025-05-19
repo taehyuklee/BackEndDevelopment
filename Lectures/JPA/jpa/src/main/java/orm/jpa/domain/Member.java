8d1d4ec93c99b101eaf6d3ba5d757711d6507fed
TAYLEE <89365465+TAEHYUKLEE@users.noreply.github.com>
2025-05-19 12:47:53 +0900

    commit) hibernate jpa

---
+++ Lectures/JPA/jpa/src/main/java/orm/jpa/domain/Member.java
@@ -0,0 +1,39 @@
+package orm.jpa.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "GO")
+public class Member {
+
+    @Id
+    private Long id;
+    private String name;
+
+    public Member(){
+
+    }
+
+    public Member(Long id, String name){
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id){
+        this.id = id;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+}
