dc751c516c9a5b5ffd2d2ed70f740864e91cf9b2
TAYLEE <89365465+TAEHYUKLEE@users.noreply.github.com>
2025-05-20 01:13:52 +0900

    commit) securityConfig

--- auth-core/src/main/java/com/taehyuk/_auth/auth_core/member/domain/Member.java
+++ auth-core/src/main/java/com/taehyuk/_auth/auth_core/member/domain/Member.java
@@ -3,14 +3,21 @@ package com.taehyuk._auth.auth_core.member.domain;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Getter
 @Entity
 @NoArgsConstructor(access = AccessLevel.PACKAGE)
 // PACKAGE - Private & Package
-public class Member {
+public class Member implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY) //DB에게 맡긴다. (AUTO
@@ -25,6 +32,51 @@ public class Member {
 
     @Column(nullable = false)
     private String role;
+
+    @Column(nullable = false)
+    private String grade;
+
+    @Builder
+    public Member(Long id, String email, String password, String role, String grade) {
+        this.id =id;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+        this.grade = grade;
+    }
+
+
+    // 권한을 반환한다.
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(this.role), new SimpleGrantedAuthority(this.grade));
+//        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
 
 
 }
